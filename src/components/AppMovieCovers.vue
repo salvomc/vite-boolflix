b6365189173d42d01e5ba49a3a9845dd56a9f11a
salvomc <smcrwrk@gmail.com>
2023-06-27 15:47:35 +0200

    rating feature added

--- src/components/AppMovieCovers.vue
+++ src/components/AppMovieCovers.vue
@@ -8,9 +8,21 @@ import { store } from '../store';
         return {
           store   
         }
-    },
+    },methods:
+    {
+        obtainRating(){
+            let rating = Math.ceil(this.MyMovies.vote_average / 2)
+            return rating
+        },
+        obtainEmptyStars(){
+            let empty_stars = 5 - (Math.ceil(this.MyMovies.vote_average / 2))
+            return empty_stars
+        }
+        },mounted(){
+            this.obtainRating()
+            this.obtainEmptyStars()
+        }
     }
-    
 </script>
 
 <template>
@@ -19,8 +31,9 @@ import { store } from '../store';
         <h1>{{ MyMovies.name }} {{ MyMovies.title }}</h1>
         <p>{{ MyMovies.original_name }} {{ MyMovies.original_title }}</p>
         <div><img class="flag" :src="'../../node_modules/country-flag-icons/flags/1x1/' + MyMovies.original_language.toUpperCase() + '.svg'" alt="IMMAGINE NON TROVATA"></div>
-        <span>{{ MyMovies.overview }}</span>
-        <p>{{ MyMovies.vote_average }}</p>
+        <p>{{ MyMovies.overview }}</p>
+        <span v-for="rating in obtainRating()" :key="rating"><i class="fa-solid fa-star" style="color: goldenrod;"></i></span>
+        <span v-for="empty_stars in obtainEmptyStars()" :key="empty_stars"><i class="fa-regular fa-star" style="color: goldenrod;"></i></span>
     </div>
 
 </template>
